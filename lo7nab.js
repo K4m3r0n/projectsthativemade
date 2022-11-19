745aa900f47c63b036b75267bb0c021c60a44568
Daliawashere <106355628+Daliawashere@users.noreply.github.com>
2022-11-18 19:07:17 -0800

    Create lo7nab.js

---
+++ lo7nab.js
@@ -0,0 +1,2 @@
+//Not mine, its my friends but its cool. CREDITS: Jaspertherock (bit.ly/lonab)
+javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='https://cdn.jsdelivr.net/gh/Jaztak/lo7NAB/lo7NAB_v0.3.js';void(0);
